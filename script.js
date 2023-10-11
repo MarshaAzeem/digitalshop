33e7bbb678c6bc8b2711fb8416ae785b1264d8a4
MarshaAzeem <143182851+MarshaAzeem@users.noreply.github.com>
2023-10-11 06:55:43 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,157 @@
+// Blog boxes
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickableBox1= document.getElementById("clickable-box1");
+
+    clickableBox1.addEventListener("click", function() {
+       
+        window.location.href = "blog1.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickableBox2= document.getElementById("clickable-box2");
+
+    clickableBox2.addEventListener("click", function() {
+        
+        window.location.href = "blog2.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickableBox3= document.getElementById("clickable-box3");
+
+    clickableBox3.addEventListener("click", function() {
+       
+        window.location.href = "blog3.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickableBox4= document.getElementById("clickable-box4");
+
+    clickableBox4.addEventListener("click", function() {
+        
+        window.location.href = "blog4.html"; 
+    });
+});
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickableBox5= document.getElementById("clickable-box5");
+
+    clickableBox5.addEventListener("click", function() {
+       
+        window.location.href = "blog5.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickableBox6= document.getElementById("clickable-box6");
+
+    clickableBox6.addEventListener("click", function() {
+        
+        window.location.href = "blog6.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickableBox7= document.getElementById("clickable-box7");
+
+    clickableBox7.addEventListener("click", function() {
+     
+        window.location.href = "blog7.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickableBox8= document.getElementById("clickable-box8");
+
+    clickableBox8.addEventListener("click", function() {
+       
+        window.location.href = "blog8.html"; 
+    });
+});
+
+// shop boxes
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickable1= document.getElementById("clickable1");
+
+    clickable1.addEventListener("click", function() {
+       
+        window.location.href = "p1.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickable2= document.getElementById("clickable2");
+
+    clickable2.addEventListener("click", function() {
+       
+        window.location.href = "p2.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickable3= document.getElementById("clickable3");
+
+    clickable3.addEventListener("click", function() {
+       
+        window.location.href = "p3.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickable4= document.getElementById("clickable4");
+
+    clickable4.addEventListener("click", function() {
+       
+        window.location.href = "p4.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickable5= document.getElementById("clickable5");
+
+    clickable5.addEventListener("click", function() {
+       
+        window.location.href = "p5.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickable6= document.getElementById("clickable6");
+
+    clickable6.addEventListener("click", function() {
+       
+        window.location.href = "p6.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickable7= document.getElementById("clickable7");
+
+    clickable7.addEventListener("click", function() {
+       
+        window.location.href = "p7.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickable8= document.getElementById("clickable8");
+
+    clickable8.addEventListener("click", function() {
+       
+        window.location.href = "p8.html"; 
+    });
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    const clickable9= document.getElementById("clickable9");
+
+    clickable9.addEventListener("click", function() {
+       
+        window.location.href = "p9.html"; 
+    });
+});
